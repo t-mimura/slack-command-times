b1609709deae4ed747e53effd31f482b317fdd8f
MIMURA Tomohiro <t-mimura@sakura.ad.jp>
2016-12-06 09:56:00 +0900

    add button to add to slack

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ const env = {
   port: 3000
 };
 
+const scopes = ['commands'];
+const addToSlackButton = `
+  <a href="https://slack.com/oauth/authorize?scope=${scopes.join(',')}&client_id=${secrets.clientId}">
+    <img alt="Add to Slack" height="40" width="139"
+      src="https://platform.slack-edge.com/img/add_to_slack.png"
+      srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+  </a>`;
+
 const controller = botkit.slackbot({
   debug: false
 }).configureSlackApp({
   clientId: secrets.clientId,
   clientSecret: secrets.clientSecret,
-  scopes: ['commands']
+  scopes: scopes
 });
 
 controller.setupWebserver(env.port, (err, webserver) => {
+  webserver.get('/', (req, res) => {
+    res.send(addToSlackButton);
+  });
   controller
-    .createHomepageEndpoint(controller.webserver)
     .createOauthEndpoints(controller.webserver, (err, req, res) => {
       if (err) {
         res.status(500).send('Error: ' + JSON.stringify(err));
